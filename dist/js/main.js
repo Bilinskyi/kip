bbb9fcfe1552a85c1ade680c77a3b0c37340acd8
Taras Bilinskyi <1unitedcrew@gmail.com>
2016-10-06 13:16:23 +0300

    v2

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,33 @@
 
 $(document).ready(function(){
 
+	$(window).on('load resize', function() {
+		menu_change();
+	});
+	$(window).resize(function() {
+		resizeSlider();
+		resizeSlider2();
+	});
+
+
+	$('.form-search').on('click', function(e) {
+		$(this).addClass('act');
+	});
+	$('.form-search').mouseleave(function() {
+		$(this).removeClass('act');
+	});
+
+
+	var menu_top = $('.top-menu li');
+	function menu_change() {
+		if ($(window).width() < 753) {
+			$('.navigation').append(menu_top);
+		} else {
+			$('.top-menu').append(menu_top);
+		};
+	};
+
+
 	$('.cmn-toggle-switch').on('click', function(e) {
 		e.preventDefault(); 
 		$(this).toggleClass('active');
@@ -19,78 +46,179 @@ $(document).ready(function(){
 			$(this).next().slideToggle();
 		});
 		
-	}
+	};
 
-
-	$('.phone-main').on('click', function(e) {
-		e.preventDefault(); 
-		$(this).next().slideToggle();
-		$(this).toggleClass('act');
-	});
 
 	$('.phone-hide span').on('click', function(e) {
 		e.preventDefault(); 
-		$(this).closest('.phone-wrapp').find('.phone-main').removeClass('act');
 		var old = $('.phone-display').text();
 		$('.phone-display').text($(this).text());
+		$('.phone-wrapp + a').attr( 'href', 'tel:' + $(this).text() );
 		$(this).text(old);
-		$('.phone-hide').slideUp();
-	});
-
-	$('.owl-carousel').owlCarousel({
-		singleItem: true,
-		navigation : true, 
-		slideSpeed : 300,
-		paginationSpeed : 400,
-		navigationText: false,
-		autoPlay: 20000,
-		mouseDrag : false,
-		pagination: false,
-		touchDrag : false
-	});
-
-$('.slider-2').owlCarousel({
-		items : 10,
-    itemsDesktop : [1399,5],
-    itemsDesktopSmall : [980,4],
-    itemsTablet: [768,3],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    singleItem : false,
-		pagination : false, 
-		navigation : true, 
-		slideSpeed : 300,
-		paginationSpeed : 400,
-		navigationText: false,
-		autoPlay: 20000,
-		mouseDrag : false,
-		touchDrag : false
 	});
 
 
-$('.slider-3').owlCarousel({
-		items : 3,
-    itemsDesktop : [1399,3],
-    itemsDesktopSmall : [976,2],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [680,1],
-    singleItem : false,
-		pagination : false, 
-		navigation : true, 
-		slideSpeed : 300,
-		paginationSpeed : 400,
-		navigationText: false,
-		autoPlay: 20000,
-		mouseDrag : false,
-		touchDrag : false
+	$('.owl-carousel').bxSlider({
+		responsive:true,
+		adaptiveHeight: true,
+		minSlides: 1,
+		maxSlides: 1,
+		slideMargin: 0,
+		moveSlides: 1,
+		pager: false,
+		autoHover: false,
+		speed: 500,
+		pause: 3000,
+		controls: true,
+		nextText:'<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+		prevText:'<i class="fa fa-chevron-left" aria-hidden="true"></i>'
 	});
 
 
-$('.flex-footer-item h4').on('click', function(e) {
- e.preventDefault(); 
- $(this).next().slideToggle();
-});
+	function countItems() {
+		var width = $(window).width();
+		if (width < 400) {
+			item = 1;
+		} else if (width < 600) {
+			item = 2;
+		} else if (width < 768) {
+			item = 3;
+		} else if (width < 1000) {
+			item = 4;
+		} else if (width < 1200) {
+			item = 6;
+		} else if (width < 1320) {
+			item = 7;
+		} else if (width < 1600){
+			item = 8;
+		} else {
+			item = 10;
+		}
+	};
+	countItems();
+
+	var carousel = $('.slider-2').bxSlider({
+		responsive:true,
+		adaptiveHeight: true,
+		minSlides: item,
+		maxSlides: item,
+		responsive: true,
+		slideWidth: 140,
+		slideMargin: 5,
+		moveSlides: 1,
+		pager: false,
+		autoHover: false,
+		speed: 500,
+		pause: 3000,
+		controls: true,
+		nextText:'<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+		prevText:'<i class="fa fa-chevron-left" aria-hidden="true"></i>'
+	});
+
+	function resizeSlider() {
+		countItems();
+		carousel.reloadSlider({
+			responsive:true,
+			adaptiveHeight: true,
+			minSlides: item,
+			maxSlides: item,
+			responsive: true,
+			slideWidth: 140,
+			slideMargin: 5,
+			moveSlides: 1,
+			pager: false,
+			autoHover: false,
+			speed: 500,
+			pause: 3000,
+			controls: true,
+			nextText:'<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+			prevText:'<i class="fa fa-chevron-left" aria-hidden="true"></i>'
+		});
+	}
+
+
+
+
+	function countItems2() {
+		var width = $(window).width();
+		if (width < 753) {
+			item2 = 1;
+		} else if (width < 1005) {
+			item2 = 2;
+		} else if (width >= 900) {
+			item2 = 3;
+		};
+	};
+	countItems2();
+	var carousel2 = $('.slider-3').bxSlider({
+		responsive:true,
+		adaptiveHeight: true,
+		minSlides: item2,
+		maxSlides: item2,
+		infiniteLoop: false,
+		slideWidth: 1000,
+		responsive: true,
+		slideMargin: 0,
+		moveSlides: 1,
+		pager: false,
+		autoHover: false,
+		speed: 500,
+		pause: 3000,
+		controls: true,
+		nextText:'<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+		prevText:'<i class="fa fa-chevron-left" aria-hidden="true"></i>'
+	});
+
+	function resizeSlider2() {
+		countItems2();
+		carousel2.reloadSlider({
+			responsive:true,
+			adaptiveHeight: true,
+			minSlides: item2,
+			maxSlides: item2,
+			infiniteLoop: false,
+			slideWidth: 1000,
+			responsive: true,
+			slideMargin: 0,
+			moveSlides: 1,
+			pager: false,
+			autoHover: false,
+			speed: 500,
+			pause: 3000,
+			controls: true,
+			nextText:'<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+			prevText:'<i class="fa fa-chevron-left" aria-hidden="true"></i>'
+		});
+	}
+
+
+
+
+
+
+	// $('.slider-3').owlCarousel({
+	// 	items : 3,
+	// 	itemsDesktop : [1399,3],
+	// 	itemsDesktopSmall : [976,2],
+	// 	itemsTablet: [768,2],
+	// 	itemsTabletSmall: false,
+	// 	itemsMobile : [680,1],
+	// 	singleItem : false,
+	// 	pagination : false, 
+	// 	navigation : true, 
+	// 	slideSpeed : 300,
+	// 	paginationSpeed : 400,
+	// 	navigationText: false,
+	// 	autoPlay: 20000,
+	// 	mouseDrag : false,
+	// 	touchDrag : false
+	// });
+
+
+	$('.flex-footer-item h4').on('click', function(e) {
+		e.preventDefault(); 
+		$(this).next().slideToggle();
+	});
 
 // $('header a[href^="#"]').on('click', function(event) {
 
